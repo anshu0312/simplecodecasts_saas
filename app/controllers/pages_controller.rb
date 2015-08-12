0c3441ac30ecded495ddc4d45a841e85dc903be8
Divyanshu Rathore <divyanshu.rathore1234@gmail.com>
2015-08-12 22:02:54 +0000

    Modified sign up form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @basic = Plan.find(1)
-    @pro = Plan.find(2)
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
 
   def about
